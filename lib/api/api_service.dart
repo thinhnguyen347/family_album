453733f66c9e9e94553920c4d1c40f6e77dc78de
Nguyễn Thịnh <110882542+nguyen-thinh109@users.noreply.github.com>
2023-02-17 17:35:20 +0700

    vkk

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert' as convert;
-import 'dart:convert';
 import 'dart:core';
 
 import 'package:flutter/foundation.dart';
@@ -8,10 +7,10 @@ import 'package:home_album/models/photo_details.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const baseURL = "https://family-album-svr.onrender.com";
+  static const baseURL = "family-album-svr.onrender.com";
 
   Future<List<PhoneDetails>> getPhoneNumber() async {
-    var url = Uri.parse("https://family-album-svr.onrender.com/phone");
+    var url = Uri.https(baseURL, "/phone");
     http.Response response;
     List<PhoneDetails> phoneList = [];
 
@@ -22,24 +21,19 @@ class ApiService {
     }
 
     if (response.statusCode == 200) {
-
-      var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
       var data = jsonResponse['results'];
-
-      if (kDebugMode) {
-        print(response.body.toString());
-      }
 
       for (int i = 0; i < data.length; i++) {
         PhoneDetails phoneItem =
             PhoneDetails.fromJson(jsonResponse['results'][i]);
         phoneList.add(phoneItem);
+        if (kDebugMode) {
+          print(phoneItem.phoneNumber);
+        }
       }
-
-
     }
-
-
 
     return phoneList;
   }
