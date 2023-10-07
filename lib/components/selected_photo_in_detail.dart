6c50bcde01ea31e46c51f7550646c45f2d1e6a26
nguyen-thinh109 <thinh.nguyenduc1@vdigital.vn>
2023-10-07 23:36:35 +0700

    aha

--- lib/components/selected_photo_in_detail.dart
+++ lib/components/selected_photo_in_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:home_album/models/photo_details.dart';
+import 'package:flutter/services.dart';
 
 class SelectedPhotoInDetail extends StatefulWidget {
   final PhotoDetails selectedPhoto;
@@ -13,9 +14,14 @@ class SelectedPhotoInDetail extends StatefulWidget {
 }
 
 class _MovieInDetailState extends State<SelectedPhotoInDetail> {
+  late bool isMiniSizedView;
+
   @override
   void initState() {
     super.initState();
+    setState(() {
+      isMiniSizedView = true;
+    });
   }
 
   @override
@@ -26,6 +32,8 @@ class _MovieInDetailState extends State<SelectedPhotoInDetail> {
       extendBodyBehindAppBar: false,
       appBar: AppBar(
         elevation: 0,
+        title: Text(selectedPhoto.title),
+        centerTitle: true,
         backgroundColor: Colors.black87,
         iconTheme: const IconThemeData(color: Colors.white),
         actions: [
@@ -42,55 +50,86 @@ class _MovieInDetailState extends State<SelectedPhotoInDetail> {
       ),
       body: Stack(
         children: [
-          Image.network(selectedPhoto.link,
-              fit: BoxFit.cover,
-              height: MediaQuery
-                  .of(context)
-                  .size
-                  .height,
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width),
-          Container(
-              padding: const EdgeInsets.all(16),
-              height: MediaQuery
-                  .of(context)
-                  .size
-                  .height,
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width,
-              color: Colors.black45.withOpacity(0.8)),
           SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.network(selectedPhoto.link, fit: BoxFit.fitWidth),
-                const SizedBox(height: 24),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Text(
-                    'Thông tin ảnh:',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Center(
-                      child: selectedPhoto.title != ''
-                          ? Text(selectedPhoto.title,
-                          textAlign: TextAlign.center)
-                          : const Text('Đang cập nhật...',
-                          textAlign: TextAlign.center)),
-                ),
-                const SizedBox(height: 24),
-              ],
+            scrollDirection: Axis.horizontal,
+            child: Image.network(
+              selectedPhoto.link,
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height,
             ),
           ),
+          isMiniSizedView
+              ? Container(
+                  padding: const EdgeInsets.all(16),
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.black45.withOpacity(0.8))
+              : const SizedBox(width: 0, height: 0),
+          isMiniSizedView
+              ? SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(selectedPhoto.link, fit: BoxFit.fitWidth),
+                      const SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  OutlinedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isMiniSizedView = false;
+                                      });
+                                    },
+                                    style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                          width: 1.0, color: Colors.white),
+                                      fixedSize: const Size(120, 40),
+                                    ),
+                                    child: const Text('Xem cỡ lớn',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            height: 1.5)),
+                                  ),
+                                ]),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  OutlinedButton(
+                                    onPressed: () {
+                                      Clipboard.setData(ClipboardData(
+                                          text: selectedPhoto.link));
+                                    },
+                                    style: OutlinedButton.styleFrom(
+                                      side: const BorderSide(
+                                          width: 1.0, color: Colors.white),
+                                      fixedSize: const Size(120, 40),
+                                    ),
+                                    child: const Text('Copy link',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 14,
+                                            height: 1.5)),
+                                  ),
+                                ]),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              : const SizedBox(width: 0, height: 0),
         ],
       ),
     );
